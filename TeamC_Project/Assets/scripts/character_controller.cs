ce80ce1fe2a03a0b6665ca3a67577ec2ccf70ddb
salihyusuf6 <148462611+salihyusuf6@users.noreply.github.com>
2024-04-08 19:32:25 +0300

    karakter ana hareketler tamam

--- TeamC_Project/Assets/scripts/character_controller.cs
+++ TeamC_Project/Assets/scripts/character_controller.cs
@@ -1,25 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class main_character : MonoBehaviour
 {
-    private Rigidbody2D rigbody2D;
+    private bool jumping;
+    [SerializeField]private float speed;
+    [SerializeField]private float jumpforce;
+    private Rigidbody2D rigbody;
     private BoxCollider2D boxCollider;
+    private float moveX;
+    private float moveY;
+    private Animator animator;
+    private SpriteRenderer pSprite;
+    private Vector2 movedir;
     // Start is called before the first frame update
     void Start()
     {
-        rigbody2D.GetComponent<Rigidbody2D>();
-        boxCollider.GetComponent<BoxCollider2D>();
+        pSprite = GetComponent<SpriteRenderer>();
+        rigbody=GetComponent<Rigidbody2D>();
+        boxCollider=GetComponent<BoxCollider2D>();
+        animator=GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+    animator.SetBool("running", false);
+     moveX = 0f;
+     moveY = 0f;
+     if (Input.GetKeyDown(KeyCode.W))
+     {
+        animator.SetBool("on_air",true);
+        jumping = true;
+        moveY=+1f;
+     }
+     if (Input.GetKey(KeyCode.D))
+     {
+        pSprite.flipX = false;
+        moveX=+1f;
+        animator.SetBool("running", true);
+     }
+     if (Input.GetKey(KeyCode.A))
+     {
+        animator.SetBool("running", true);
+        moveX=-1f;
+        pSprite.flipX=true;
+     }
+     if (Input.GetKeyDown(KeyCode.Mouse1))
+     {
+        animator.SetBool("attacking",true);
+     }
+     movedir = new Vector2(moveX, moveY);
+     animator.SetFloat("moveX", Mathf.Abs(moveX));
+     animator.SetFloat("moveY",rigbody.velocity.y);
     }
-    private void fixedupdate()
+    private void FixedUpdate() {
+        if (jumping)
+        {
+            jumping=false;
+            rigbody.velocity = new Vector2(rigbody.velocity.x, jumpforce);
+        }
+        rigbody.velocity = new Vector2(moveX*speed, rigbody.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "platform")
+        {
+            animator.SetBool("on_air", false);
+        }
+    }
+    private void attack_quit()
     {
-        
+        animator.SetBool("attacking",false);
     }
 }
